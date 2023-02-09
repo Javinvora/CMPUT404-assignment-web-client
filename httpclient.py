3c688346fbfe5d2602986bbf5a4e8a290a29fa00
Javin Vora <71691153+Javinvora@users.noreply.github.com>
2023-02-09 16:27:11 -0700

    Made changes in the GET() AND THE POST() functions

--- httpclient.py
+++ httpclient.py
@@ -18,11 +18,20 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+"""
+Documentation of the REFERENCES Used:
+I understood and learnt the conecept of parsing the input URL through urlparse to obtain the Host, Port and Path
+and understood how to convert the argumenst present in the form of a dictiinary into a query string thorugh urlencode
+from this resource: Author: Python Software Foundation, Date: Feb 09, 2023 (last update), Title: "urllib.parse — Parse URLs into components", Online Resource:https://docs.python.org/3/library/urllib.parse.html
+I applied my understanding from the above website into writing the code.
+"""
 import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+# importing the necessary libraries from the urllib.parse that are useful in my code
+from urllib.parse import urlparse
+from urllib.parse import urlencode
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -34,7 +43,7 @@ class HTTPResponse(object):
 
 class HTTPClient(object):
     #def get_host_port(self,url):
-
+    
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
@@ -68,13 +77,98 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        # Initialising an int variable for the Content length
+        i = 0
+        # parsing the url recieved in the function to obtain path, Host Name and the Port Number
+        main_url = urlparse(url)
+        # obtaining the path of the main url
+        main_path = main_url.path
+        # if the path is missing in the main URL
+        if (main_path == ""):
+            # adding the default path
+            main_path = "/"
+
+        # obtaining the Host name of the main url
+        main_host = main_url.hostname
+        # obtaining the Port Number of the main url
+        main_port = main_url.port
+        # if the port field is empty in the main url
+        if (main_port == None):
+            # set the port to the default port of HTTP
+            main_port = 80
+
+        # connecting to the socket
+        self.connect(main_host, main_port)
+        # creating the message request that will be sent via the sendall() function
+        main_message = "GET {} HTTP/1.1\r\nHost: {}\r\nUser-Agent: Safari\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {}\r\nConnection: close\r\n\r\n[]".format(main_path, main_host, i)
+        # sending the message created above via the sendall() function
+        self.sendall(main_message)
+        main_socket = self.socket
+        # obtaining the message recieved from the server
+        recv_message = self.recvall(main_socket)
+        main_code = recv_message.split(" ")
+        code = main_code[1]
+        # obtaining the code from the message recieved 
+        code = int (code)
+        main_body = recv_message.split("\r\n\r\n")
+        # obtaining the body from the message recieved 
+        body = main_body[-1]
+        # closing the socket that we connected above
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        # Initialising an int variable for the Content length
+        i = 0
+        # parsing the url recieved in the function to obtain path, Host Name and the Port Number
+        main_url = urlparse(url)
+        # obtaining the path of the main url
+        main_path = main_url.path
+        # if the path is missing in the main URL
+        if (main_path == ""):
+            main_path = "/"
+
+        # obtaining the Host name of the main url
+        main_host = main_url.hostname
+        # obtaining the Port Number of the main url
+        main_port = main_url.port
+        # if the port field is empty in the main url
+        if (main_port == None):
+            # set the port to the default port of HTTP
+            main_port = 80
+        
+        # connecting to the socket
+        self.connect(main_host, main_port)
+        # if the message does not have any arguments
+        if (args == None):
+            # Since there are no argumentsm it is almost similar to a GET request
+            # creating the message request that will be sent via the sendall() function
+            # Since there are no arguments, sending an empty array with a 0 content length
+            main_message = "POST {} HTTP/1.1\r\nHost: {}\r\nUser-Agent: Safari\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {}\r\nConnection: close\r\n\r\n[]".format(main_path, main_host, i)
+        # if the message does have any arguments
+        else:
+            # converting the arguments present in the form of a dictionary into a query string
+            # converting the arguments into 
+            main_argument = urlencode(args)
+            # computing the length of the arguments, which can be further sent as the content legth
+            main_size = len(main_argument)
+            # creating the message request that will be sent via the sendall() function
+            main_message = "POST {} HTTP/1.1\r\nHost: {}\r\nUser-Agent: Safari\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {}\r\nConnection: close\r\n\r\n{}".format(main_path, main_host, main_size, main_argument)
+        
+        # sending the message created above via the sendall() function
+        self.sendall(main_message)
+        main_socket = self.socket
+        # obtaining the message recieved from the server
+        recv_message = self.recvall(main_socket)
+        main_code = recv_message.split(" ")
+        code = main_code[1]
+        # obtaining the code from the message recieved 
+        code = int (code)
+        main_body = recv_message.split("\r\n\r\n")
+        # obtaining the body from the message recieved 
+        body = main_body[-1]
+        # closing the socket that we connected above
+        self.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
